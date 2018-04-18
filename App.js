e7ee6b84f28c1263daa948e4fd184b4ff0e3b5c6
wang <5628195@gg.com>
2018-04-18 17:50:25 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Ionicons} from '@expo/vector-icons'
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+          <Ionicons name="ios-pizza" color="red" size={100}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/AddEntry.js
@@ -0,0 +1,8 @@
+import React,{ Component }from 'react'
+import { View, Text } from 'react-native'
+import {getMetricMetaInfo} from '../utils/helpers'
+
+
+export default class AddEntry extends Component {
+	
+}
